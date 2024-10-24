6fc65bcb9266899821fb50b9bdf8e13b55d5cc56
ibrahimfnk <ibrahimkadiri.uae@gmail.com>
2024-10-24 23:00:53 +0530

    schema changes, email validation message

--- app/app.py
+++ app/app.py
@@ -27,7 +27,6 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    favorites = db.relationship('Favorite', backref='user', lazy=True)
     trips = db.relationship('Trip', backref='user', lazy=True)
 
 class Location(db.Model):
@@ -36,14 +35,10 @@ class Location(db.Model):
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
     location_type = db.Column(db.String(10), nullable=False)
-    favorites = db.relationship('Favorite', backref='location', lazy=True)
     start_trips = db.relationship('Trip', foreign_keys='Trip.start_location_id', backref='start_location', lazy=True)
     end_trips = db.relationship('Trip', foreign_keys='Trip.end_location_id', backref='end_location', lazy=True)
 
-class Favorite(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=False)
+
 
 class Trip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
